45d0f47f2ec5a62c205fca790ef262085287efbc
HYSLXM <3095507719@qq.com>
2016-09-09 12:07:06 +0800

    2

---
+++ 2048/main.js
@@ -0,0 +1,85 @@
+$(function () {
+    newgame();
+});
+
+function newgame() {
+    //初始化棋盘格
+    init();
+    //在随机两个格子生成数字
+    generateOneNumber();
+    generateOneNumber();
+}
+var board = new Array();
+function init() {
+    //i表示4乘4的格子中的行
+    for (var i = 0; i < 4; i++) {
+        board[i] = new Array();
+        for (var j = 0; j < 4; j++) {
+            board[i][j] = 0;
+            var gridCell = $("#grid-cell-" + i + "-" + j);
+            gridCell.css("top", getPosTop(i, j));
+            gridCell.css("left", getPosLeft(i, j));
+        }
+    }
+    updateBoardView();
+}
+
+function updateBoardView() {
+    $(".number-cell").remove();
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            $("#grid-container").append("<div class='number-cell' id='number-cell-" + i + "-" + j + "'></ div > ")
+            var numberCell = $("#number-cell-" + i + "-" + j);
+            if (board[i][j] == 0) {
+                numberCell.css("width", "0px");
+                numberCell.css("height", "0px");
+                numberCell.css("top", getPosTop(i, j) + 50);
+                numberCell.css("left", getPosLeft(i, j) + 50);
+            }
+            else {
+                numberCell.css("width", "100px");
+                numberCell.css("height", "100px");
+                numberCell.css("top", getPosTop(i, j))
+                numberCell.css("left", getPosLeft(i, j))
+                nunberCell.css("background-color", getNumberBackgroundColor(boar[i][j]));
+                numberCell.css("color", getNumberColor(board[i][j]));
+                numbercell.text(board[i][j]);
+            }
+        }
+    }
+    $(".number-cell").css("line-height", "100px");
+    $(".number-cell").css("font-size", "60px");
+}
+function generateOneNumber() {
+    if (nospace(board)) {
+        return false;
+    }
+    return true;
+}
+
+function generateOneNumber() {
+    var randx = parseInt(Math.floor(Math.random() * 4))
+    var randy = parseInt(Math.floor(Math.random() * 4))
+    while (true) {
+        if (board[randx][randy] == 0) {
+            break;
+        }
+        var randx = parseInt(Math.floor(Math.random() * 4))
+        var randy = parseInt(Math.floor(Math.random() * 4))
+    }
+    var randNumber = Math.random() < 0.5 ? 2 : 4;
+    board[randx][randy] = randNumber;
+    ShowNumberWithAnimation(randx,randy,randNumber);
+}
+
+
+
+
+
+
+
+
+
+
+
+
